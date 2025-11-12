8d803ed1a1e2be36c678aa8473d74a2f9e376ee2
leozamarron <zamarronleo4@gmail.com>
2025-11-12 03:09:25 -0600

    models

--- tienda/app/Models/Compra.php
+++ tienda/app/Models/Compra.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class DetalleCarrito extends Model
+class Compra extends Model
 {
     use SoftDeletes;
 
